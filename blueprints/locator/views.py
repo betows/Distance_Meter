9bbd6ace3bd045684c9ff8c4cbda8ab6cb69327a
Roberto amaral <robertoamaral56@gmail.com>
2021-08-11 11:59:50 -0300

    styling and improvements

--- blueprints/locator/views.py
+++ blueprints/locator/views.py
@@ -2,13 +2,14 @@ import flask
 from flask import Blueprint, render_template, request
 from geopy.distance import geodesic
 
+
 locator = Blueprint('locator', __name__, )
 
 
 @locator.route("/locator", methods=["GET", "POST"])
 def index():
     """
-    Calculate distance information got from the api using geodesic package and show to the user
+    Calculate distance information from the api using geodesic package and show to the user
     """
     distance = None
     destination_latitude = None
@@ -25,5 +26,5 @@ def index():
         distance = round(geodesic(coords_1, coords_2).km)
 
     return render_template("locator/index.html", distance=distance,
-                                                 destination_longitude=destination_longitude,
-                                                 destination_latitude=destination_latitude)
+                           destination_longitude=destination_longitude,
+                           destination_latitude=destination_latitude)
---
+++ distanceapi.py
@@ -0,0 +1,41 @@
+# importing required libraries
+import requests, json
+import googlemaps
+
+
+# enter your api key here
+api_key = 'AIzaSyCXt-eENymUYzhr8TAqhnpwx1p5y9swyW0'
+
+# Take source as input
+source = input("Coordenadas origem:")
+
+# Take destination as input
+dest = input("Coordenadas destino:")
+
+# url variable store url
+url = 'https://maps.googleapis.com/maps/api/distancematrix/json?'
+
+# Get method of requests module
+# return response object
+r = requests.get(url + 'origins=' + source +
+             '&destinations=' + dest +
+             '&key=' + api_key)
+
+# json method of response object
+# return json format result
+x = r.json()
+
+# by default driving mode considered
+
+# print the value of x
+print(x)
+
+
+# Requires API key
+gmaps = googlemaps.Client(key='AIzaSyCXt-eENymUYzhr8TAqhnpwx1p5y9swyW0')
+
+# Requires cities name
+my_dist = gmaps.distance_matrix('Delhi', 'Mumbai')['rows'][0]['elements'][0]
+
+# Printing the result
+print(my_dist)
